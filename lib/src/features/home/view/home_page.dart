0438bccc72993b303d4c57b74e540a1ff44fcf8f
VictorDouglasFernandes <victordouglasfernandes@gmail.com>
2022-07-17 22:23:55 -0300

    🚧 Create home module

---
+++ lib/src/features/home/view/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      body: _buildBody(),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      title: const Text(
+        'Pomodoro App Bar',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.grey.shade700,
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.grey.shade700,
+            Colors.white,
+          ],
+          stops: const [
+            0.0,
+            0.95,
+          ],
+        ),
+      ),
+    );
+  }
+}
